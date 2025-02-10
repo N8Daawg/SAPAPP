aa8ff65a881e8cd5fcead9333e422d38c977489a
N8Daawg <nbeals50@gmail.com>
2025-02-09 20:48:00 -0600

    Add project files.

---
+++ SAPAPP/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Orientation = System.Windows.Controls.Orientation;
+using ProgressBar = System.Windows.Controls.ProgressBar;
+
+
+namespace SAPAPP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            progbar.IsIndeterminate = true;
+
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            progbar.IsIndeterminate = false;
+        }
+    }
+}
