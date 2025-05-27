b9a64b295ac441f9f22771ec405bc42c409201f7
av1452 <aidan.vazquez@valpo.edu>
2025-05-27 11:30:58 -0500

    Started Implementation of Test Files For TI Fuel Gauge Programmer

---
+++ SAPAPP/Scripts/TIScripts.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SAPAPP.Scripts
+{
+    class TIScripts
+    {
+        private static SerialPort serialPort;
+        private const int BaudRate = 115200;
+
+        public static void Initialize()
+        {
+            // Setup the serial port communication
+            serialPort = new SerialPort("COM3", BaudRate);
+            serialPort.Open();
+
+            Console.WriteLine("Press 'f' to program fuel gauge...");
+
+            while (true)
+            {
+                char inByte = (char)serialPort.ReadByte();
+                if (inByte == 'f')
+                {
+                    ProgramFirmware();
+                }
+            }
+        }
+
+        private static void ProgramFirmware()
+        {
+            Console.WriteLine("Begin programming...");
+
+            // Simulated firmware programming logic
+            for (int i = 0; i < 10; i++) // Replace with actual firmware data processing
+            {
+                Console.WriteLine($"Writing firmware block {i}...");
+                Thread.Sleep(500); // Simulate delay for data transmission
+            }
+
+            Console.WriteLine("Programming PASSED.");
+        }
+    }
+}
